88ea5c3186668f0a97355b12448e652c7107d6ea
pzjiang <pzjiang01@hotmail.com>
2022-02-08 14:36:47 -0500

    create controller for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,15 @@ class User < ApplicationRecord
   
   validates_uniqueness_of :username
   validates:username, length: {in: 4..20}
+  validates :username, presence: true, uniqueness: { case_sensitive: false }
+  validate :validate_username
+  validates:email, format: {with: /\b[A-Z0-9._%a-z\-]+@purdue\.edu\z/, message: "must be a purdue.edu account"}
+
+  def validate_username
+    if User.where(email: username).exists?
+      errors.add(:username, :invalid)
+    end
+  end
 
   attr_writer :login
 
