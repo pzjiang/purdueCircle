d51eb3cc080a6c44222b02598e2408aed1b22b1a
pzjiang <pzjiang01@hotmail.com>
2022-02-08 13:16:08 -0500

    added username to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,18 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable #, authentication_keys: [:login]
+
+  
+  
+  validates_uniqueness_of :username
+  validates:username, length: {in: 4..20}
+
+  attr_writer :login
+
+  def login
+    @login || self.username || self.email
+  end
+
+
 end
