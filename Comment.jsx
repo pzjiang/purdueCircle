83775c8552f1d5f357ae2f01fd658df4efe7c7ff
Derek Lee <leederek007@gmail.com>
2022-02-12 22:35:10 -0500

    added creating posts fields, validation
    added posts field, preliminary navigation button(waiting on postgresql), button to return back to main page for page not found

---
+++ Comment.jsx
@@ -0,0 +1,15 @@
+/**
+ * Comment UI
+ */
+
+ const Comment = () => { 
+ 
+    return (
+        <div>
+            <form className="comment-box">Comment Here</form>
+            <button className="comment-post">Post</button>
+        </div>
+    );
+}
+
+export default Comment;
