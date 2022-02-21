4dd62805595a028674df8bd479715aa4b13104d3
sylviaxliu <sylviaxliu@yahoo.com>
2022-02-21 17:23:54 -0500

    initial post ui

--- app/javascript/components/objs/Post.jsx
+++ app/javascript/components/objs/Post.jsx
@@ -6,6 +6,8 @@ import React from "react";
 
 const Post = () => { 
 
+    
+
     const countLikes = () => {
 
     };
@@ -19,7 +21,7 @@ const Post = () => {
     };
 
     return (
-        <div>
+        <div id="post">
             <h1>this will be the post header</h1>
             <div>this will be the post content, find way to make images appear as desired by user</div>
             <div>if the user owns the post, edit option appears</div>
