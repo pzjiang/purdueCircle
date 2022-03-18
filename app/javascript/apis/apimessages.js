88bb69e62200b49bb409f02403d4c4e53e07e3cb
pzjiang <pzjiang01@hotmail.com>
2022-03-18 13:39:07 -0400

    add commenting to detail use of apimessages.js

--- app/javascript/apis/apimessages.js
+++ app/javascript/apis/apimessages.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
 //origin is the logged in user, target is the other user that is being messaged
+//retrieves the messages in order of created time
+//returns single array that contains all messages
 const getMessages = payload => axios.get(`api/v1/messages/${payload.origin_id}/${payload.target_id}`);
 
 //in the payload, send in logged in user id as origin_id, who is receiving the message as target_id, and the message as body
